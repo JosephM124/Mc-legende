1c32d56d447394ad0e64dd6d1ea3bdd018a4bb21
henock <henoctumonakiese@gmail.com>
2025-07-10 10:31:09 +0100

    reglage des conflits de synchronysation

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-    require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+    require (__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
     $path = isset($_SERVER['BASE_URI']) ? $_SERVER['BASE_URI'] : '';
     require './routes/routes.php';
     
